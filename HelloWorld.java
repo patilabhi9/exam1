0f7dc8cffaf40da09918a00949300f292c5d4218
patilabhi9 <abhidpatil9@gmail.com>
2023-02-27 11:59:13 +0530

    coorected Code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,9 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello world !!!!!")
-        System.out.println("Welcome All !!!!!")
-        System.out.println("Greetings to Github !!!!!")
+        System.out.println("Hello world !!!!!");
+        System.out.println("Welcome All !!!!!");
+        System.out.println("Greetings to Github !!!!!");
     }
 
 }
